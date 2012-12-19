6b939e39c205be8db67eeaa688b44cc024b151fc
Marco Jacobasch <mj@orod.de>
2012-12-19 16:20:32 +0100

    Code formating

--- de.opalproject.vespucci.ui/src/de/opalproject/vespucci/ui/navigator/handlers/NewEnsembleHandler.java
+++ de.opalproject.vespucci.ui/src/de/opalproject/vespucci/ui/navigator/handlers/NewEnsembleHandler.java
@@ -84,12 +84,11 @@ public class NewEnsembleHandler extends AbstractHandler {
 
 					Ensemble ens = factory.createEnsemble();
 
-
-			selectetDomainObject.getChildren().add(ens);
-			ens.setName(wiz.name);
-			ens.setDescription(wiz.description);
-			ens.setDerived(false);
-			ens.setQuery(wiz.query);
+					selectetDomainObject.getChildren().add(ens);
+					ens.setName(wiz.name);
+					ens.setDescription(wiz.description);
+					ens.setDerived(false);
+					ens.setQuery(wiz.query);
 
 					// selectetDomainObject.getChildren().add(ens);
 					ens.setParent(selectetDomainObject);
