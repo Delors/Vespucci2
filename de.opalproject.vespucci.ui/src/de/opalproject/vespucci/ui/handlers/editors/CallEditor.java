a6bca0ae15401f324a84b8c9f7ab5d1ab0678e53
Marco Jacobasch <mj@orod.de>
2013-01-30 15:07:48 +0100

    Move editor handlers to own package

--- de.opalproject.vespucci.ui/src/de/opalproject/vespucci/ui/handlers/editors/CallEditor.java
+++ de.opalproject.vespucci.ui/src/de/opalproject/vespucci/ui/handlers/editors/CallEditor.java
@@ -31,7 +31,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package de.opalproject.vespucci.ui.handlers;
+package de.opalproject.vespucci.ui.handlers.editors;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
