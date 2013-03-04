b94ff4755cb2e4e9a707ec3820e6fcf4688e1bcb
Marco Jacobasch <mj@orod.de>
2013-03-04 14:06:14 +0100

    removed unused local variable

--- de.opalproject.vespucci.sliceEditor/src/de/opalproject/vespucci/sliceEditor/SliceEditorDiagramTypeProvider.java
+++ de.opalproject.vespucci.sliceEditor/src/de/opalproject/vespucci/sliceEditor/SliceEditorDiagramTypeProvider.java
@@ -35,7 +35,6 @@ package de.opalproject.vespucci.sliceEditor;
 
 import org.eclipse.graphiti.dt.AbstractDiagramTypeProvider;
 import org.eclipse.graphiti.tb.IToolBehaviorProvider;
-import org.eclipse.graphiti.ui.editor.DefaultMarkerBehavior;
 
 /**
  * Provides the diagram type. FeatureProvider and ToolBehaviorProvider have to
@@ -66,8 +65,6 @@ public class SliceEditorDiagramTypeProvider extends AbstractDiagramTypeProvider 
 			toolBehaviorProviders = new IToolBehaviorProvider[] { new SliceEditorToolBehaviorProvider(
 					this) };
 		}
-
-		DefaultMarkerBehavior adf = null;
 		return toolBehaviorProviders;
 	}
 
