4673171f03ef0a13e692d1ac97598ca8102de3cf
Marco Jacobasch <mj@orod.de>
2013-03-08 10:28:59 +0100

    Added ActionBarContributor to enable undo/redo

---
+++ de.opalproject.vespucci.sliceEditor/src/de/opalproject/vespucci/sliceEditor/SliceEditorActionBarContributor.java
@@ -0,0 +1,17 @@
+package de.opalproject.vespucci.sliceEditor;
+
+import org.eclipse.graphiti.ui.editor.DiagramEditorActionBarContributor;
+
+/**
+ * ActionbarContributor for actions like undo and redo.
+ * 
+ * Simple Subclass of {@link DiagramEditorActionBarContributor} with currently
+ * no changes, but in case the should sometimes be needed.
+ * 
+ * @author Marco Jacobasch
+ * 
+ */
+public class SliceEditorActionBarContributor extends
+		DiagramEditorActionBarContributor {
+
+}
